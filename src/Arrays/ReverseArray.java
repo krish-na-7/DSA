6bbfeb5007c9c9a3916c0aa9881418d35ba246ad
Sai Krishna <kvskrishna712@gmail.com>
2021-08-27 19:25:28 +0530

    Adding Reverse Array

---
+++ src/Arrays/ReverseArray.java
@@ -0,0 +1,38 @@
+package Arrays;
+/*
+     Time Complexity : O(n)
+     Space Complexity : O(1)
+ */
+public class ReverseArray {
+    public static void reverseArray(int[] arr,int size)
+    {
+        int start = 0,end = size-1;
+
+        while(start < end)
+        {
+            arr[start] = arr[start] + arr[end];
+            arr[end] = arr[start] - arr[end];
+            arr[start] = arr[start] - arr[end];
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {123,89,67,55,43,39,21,15,4,1};
+        int size = arr.length;
+        System.out.println("Original Array : ");
+        for(int i : arr)
+        {
+            System.out.print(i+ " ");
+        }
+        reverseArray(arr,size);
+        System.out.println();
+        System.out.println("Reversed Array : ");
+        for(int i : arr)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
