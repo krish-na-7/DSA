1ebb44f93d0de17fda2d7f2e829ac99bef0b9f68
krish-na-7 <kvskrishna712@gmail.com>
2022-05-07 17:29:31 +0530

    Updated BinarySearch

--- src/Arrays/BinarySearch.java
+++ src/Arrays/BinarySearch.java
@@ -1,12 +1,13 @@
 package Arrays;
 
+import java.util.Scanner;
+
 /*
      Binary Search:
      ==============
      Binary Search works on Sorted arrays either in ascending order or descending order.
 
-     Time Complexity:
-     O(log n)
+     Time Complexity:   O(log n)
 
      Fast and efficient algorithm but works on sorted search space which is monotonic.
      Monotonic means Non-decreasing or Non-increasing.
@@ -14,44 +15,41 @@ package Arrays;
      Non-decreasing => {2,6,8,34,45,77,90,101,117,129}
      Non-increasing => {123,89,67,55,43,39,21,15,4}
  */
-
 public class BinarySearch {
 
-    public static int binarySearch(int[] arr,int size,int key)
-    {
-        int start=0,end= size-1;
-        while(start<=end)
-        {
-            int mid = (start+end)/2;
-            if(arr[mid]==key)
-            {
+    static int binary_search(int array[],int n,int key){
+        //Implement binary search
+        int s = 0;
+        int e = n - 1;
+
+        while(s<=e){
+            int mid = (s+e)/2;
+
+            if(array[mid] == key){
                 return mid;
             }
-            else if(arr[mid]>key)
-            {
-                end = mid -1;
+            else if(array[mid] > key){
+                e = mid - 1;
             }
-            else
-            {
-                start = mid+1;
+            else{
+                s = mid + 1;
             }
         }
         return -1;
     }
+    public static void main(String[] args){
 
-    public static void main(String[] args)
-    {
-        int[] array = {2,6,8,34,45,77,90,101,117,129};
-        int length = array.length;
-        int key = 77;
-        int index = binarySearch(array,length,key);
-        if(index!=-1)
-        {
-            System.out.println(key+" is present at index "+index);
+        int array[] = new int[10];
+        int n = array.length;
+        Scanner scn = new Scanner(System.in);
+        int key = scn.nextInt();
+
+        int index = binary_search(array,n,key);
+        if(index!=-1){
+            System.out.println(key  + " is present at index " +  index);
         }
-        else
-        {
-            System.out.println(key+" Not Found !!!");
+        else{
+            System.out.println(key  + " is NOT Found!");
         }
     }
 }
