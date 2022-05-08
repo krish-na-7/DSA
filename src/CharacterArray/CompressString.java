22c1c9da257ed97d2b34600a3624110d524be322
krish-na-7 <kvskrishna712@gmail.com>
2022-05-08 16:05:29 +0530

    Adding CompressString

---
+++ src/CharacterArray/CompressString.java
@@ -0,0 +1,38 @@
+package CharacterArray;
+
+public class CompressString {
+    public static String compressString(String str){
+
+        int n = str.length();
+
+        String output = "";
+
+        for(int i=0;i<n; i++){
+
+            int count = 1;
+
+            while(i< n-1 && str.charAt(i+1) == str.charAt(i)){
+                count++;
+                i++;
+            }
+            output = output + str.charAt(i);
+            output += (count);
+
+        }
+        if(output.length() > str.length()){
+            return str;
+        }
+        return output;
+    }
+    public static void main(String[] args) {
+        String s1 = "aaabbccddee";
+        System.out.println(compressString(s1));
+        String s2 = "abcd";
+        System.out.println(compressString(s2));
+    }
+}
+/*
+OUTPUT:
+a3b2c2d2e2
+abcd
+ */
